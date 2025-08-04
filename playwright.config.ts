b5ba81951bdd62d5f928cf3cdfa1158edfba3f90
shrwnsan <splash.contour.0w@icloud.com>
2025-08-04 15:52:27 +0800

    Fix Playwright config to handle port re-use and align with CI server setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -64,9 +64,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: 'npx serve out -l 3000',
     url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
     timeout: 120 * 1000,
   },
 });
